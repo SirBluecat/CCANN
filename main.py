bde4f150f9be493a9612ffb5c26191f2530f08eb
Zac Brennan <zac925@gmail.com>
2024-04-19 08:47:18 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from model import *
 from matplotlib import pyplot as plt
+from torch.utils.data import TensorDataset, DataLoader
 
 # Left boundary and right boundary
 # Defines kernel size of CNN
-left_boundary, right_boundary = 5, 5
+left_boundary, right_boundary = 15, 15
 
 # Training Parameters
-learn_rate = .01 
+learn_rate = .01
 learn_rate_decay = .85 #seemed to help training 
-epochs = 5000
+epochs = 20000
 param_str = f"E{epochs}_LR{learn_rate}_LRD{learn_rate_decay}"
 
-# Load or save model
-load_model = True
-save_model = False
-model_location = "./saved_models/resnet_" + param_str
+# Load or save model and data
+load_data = False
+save_data = True
+load_model = False
+save_model = True
+model_location = "./saved_models/resnet_leakyrelu_wdecay_filter7" + param_str
+
+x_in_dataloc = "data/x_input_60_lrelu_f7.npy"
+x_out_dataloc = "data/x_output_60_lrelu_f7.npy"
 
 
 
@@ -39,28 +45,28 @@ def testnetwork(func, model, loss, u_init,xL=0, xR=2*np.pi, N_m = 600, cfl = 2*n
     N_t = len(t)
     
     u_init_tensor = torch.from_numpy(u_init.astype(np.float32)).to(device)
-    u_extend = torch.from_numpy(extend_boundary(func=func, u_temp = u_init, t=t[0]).astype(np.float32)).to(device)
+    u_extend = torch.from_numpy(extend_boundary(func=func, u_temp = u_init, t=t[0], lb=left_boundary, rb=right_boundary).astype(np.float32)).to(device)
     #u_extend = u_extend.reshape((1,610))
     #send to tensor
     
-    u_network_approx = torch.zeros(N_m, N_t) # Array whose column t is the model approx of time dt>0
-    u_exact = torch.zeros(N_m,N_t) # Array whose column t is the ground truth of time dt>0
+    u_network_approx = torch.zeros(N_m, N_t).to(device) # Array whose column t is the model approx of time dt>0
+    u_exact = torch.zeros(N_m,N_t).to(device) # Array whose column t is the ground truth of time dt>0
     u_network_approx[:,0] = u_init_tensor
     u_exact[:,0] = u_init_tensor
 
     for i in range(N_t-1):
-        u_extend = u_extend.reshape((1,610))
+        u_extend = u_extend.reshape((1,N_m+left_boundary+right_boundary))
         # u_network_approx[:,i+1]=model(u_extend) # Predicts next timestep into the future NON RESNET
         u_network_approx[:,i+1] = u_network_approx[:,i]+model(u_extend) #RESNET
-        u_extend = torch.from_numpy(extend_boundary(func=func, u_temp = u_network_approx[:,i+1], t = t[i+1]).astype(np.float32)).to(device)
+        u_extend = torch.from_numpy(extend_boundary(func=func, u_temp = u_network_approx[:,i+1], t = t[i+1],lb=left_boundary, rb=right_boundary).astype(np.float32)).to(device)
         exact_soln_temp = initial_approx(func=func, t=t[i+1])
         u_exact[:,i+1]=torch.from_numpy(exact_soln_temp.astype(np.float32)).to(device)
         #send extend_boundary to tensor
 
     L2_error = loss(u_exact,u_network_approx)
     print(L2_error)
-    plt.scatter(xx,u_network_approx[:,N_t-1].detach().numpy()) # cast to numpy
-    plt.scatter(xx,u_exact[:,N_t-1].detach().numpy())
+    plt.scatter(xx,u_network_approx[:,N_t-1].cpu().detach().numpy()) # cast to numpy
+    plt.scatter(xx,u_exact[:,N_t-1].cpu().detach().numpy())
     plt.show()
     
 
@@ -74,9 +80,16 @@ if __name__ == '__main__':
         # Kernel size is lb + rb + 1   
         model = get_network(left_boundary, right_boundary)
         
-        
-        X_input=generate_data(heat_exact, np.arange(1,31,1), lb=left_boundary, rb=right_boundary)[0] 
-        X_output = generate_data(heat_exact, np.arange(1,31,1), lb=left_boundary, rb=right_boundary)[1]
+        if load_data == True:
+            X_input = np.load(x_in_dataloc)
+            X_output = np.load(x_out_dataloc)
+        else:
+            X_input=generate_data(heat_exact, np.arange(1,31,.5), lb=left_boundary, rb=right_boundary)[0] 
+            X_output = generate_data(heat_exact, np.arange(1,31,.5), lb=left_boundary, rb=right_boundary)[1]
+            
+            if save_data == True:
+                np.save(x_in_dataloc, X_input)
+                np.save(x_out_dataloc, X_output)
 
         print("In vector" + str(X_input.shape))
         print("Out vector" +str(X_output.shape))
@@ -91,8 +104,8 @@ if __name__ == '__main__':
         
         X_in_tensor = torch.from_numpy(X_input.astype(np.float32)).to(device)
         X_out_tensor = torch.from_numpy(X_output.astype(np.float32)).to(device)
-        
-        
+        dataset = TensorDataset(X_in_tensor, X_out_tensor)
+        loader = DataLoader(dataset, batch_size=60)
         
         print("In tensor " + str(X_in_tensor.size()))
         print("Out tensor " + str(X_out_tensor.size()))
@@ -100,7 +113,9 @@ if __name__ == '__main__':
         ########### Training #############
         
         optimizer = get_optim(model, learn_rate=learn_rate)
-        losses = train_net(model, criterion, learn_rate, optimizer, learn_rate_decay, epochs, x=X_in_tensor, y= X_out_tensor)
+        # print(loader)
+        #losses = train_net(model, criterion, learn_rate, optimizer, learn_rate_decay, epochs, x=X_in_tensor, y= X_out_tensor)
+        losses = train_net(model, criterion, learn_rate, optimizer, learn_rate_decay, epochs, Data = loader)
         itter_number = np.arange(0,epochs)
         # plt.plot(itter_number, losses)
         # plt.show()
--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 
+
 # Use CUDA if available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -12,41 +13,61 @@ class CCANN(nn.Module):
     def __init__(self, conv_size):
         super(CCANN, self).__init__()
         # define each layer here
-        self.conv1 = nn.Conv1d(1, 10, conv_size, stride=1)
-        self.conv2 =  nn.Conv1d(10, 1, 1, stride = 1)
+        self.conv1 = nn.Conv1d(1, 2, 7, stride=1)
+        self.conv2 = nn.Conv1d(2, 4, 7, stride=1)
+        self.conv3 = nn.Conv1d(4, 8, 7, stride=1)
+        self.conv4 = nn.Conv1d(8, 4, 7, stride=1)
+        self.conv5 = nn.Conv1d(4, 2, 7, stride=1)
+        self.conv6 =  nn.Conv1d(2, 1, 1, stride = 1)
 
     def forward(self, x):
       # Define the forward Path of the model here
       # Convolution and pooling layers
         # Convolution Layer 1
-        x = F.tanh(self.conv1(x))
+        x = F.leaky_relu(self.conv1(x))
+        x = F.leaky_relu(self.conv2(x))
+        x = F.leaky_relu(self.conv3(x))
+        x = F.leaky_relu(self.conv4(x))
+        x = F.leaky_relu(self.conv5(x))
         # Convolution Layer 2
-        x = self.conv2(x)
+        x = self.conv6(x)
         return x
 
 # Instantiate network as net
 # Kernel size is lb + rb + 1
 def get_network(lb,rb):
     model = CCANN(lb+rb+1)
+    
+    # Initialize weights
+    # nn.init.uniform_(model.conv1.weight, a= -.001, b= .001)
+    # nn.init.uniform_(model.conv2.weight, a= -.001, b= .001)
+    
     model = model.to(device)
     return (model)
 
 def get_optim (model, learn_rate):
-    optimizer = torch.optim.SGD(model.parameters(), lr = learn_rate)
+    optimizer = torch.optim.SGD(model.parameters(), lr = learn_rate, weight_decay=.01)#10^-3 seems best
     return(optimizer)
 
 
-def train_net(model, criterion, learn_rate, optimizer, learn_rate_decay, epochs,x,y):
+def train_net(model, criterion, learn_rate, optimizer, learn_rate_decay, epochs, Data):
     losses = []
     for e in range(epochs):
         total_loss = 0.0
-        optimizer.zero_grad
-        outputs = model(x)
-        loss = criterion(outputs, y)
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-        losses.append(total_loss)
+        for batch_idx,batch in enumerate(Data):
+            x=batch[0].to(device)
+            y=batch[1].to(device)
+            # print("Size x" + str(x.size()))
+            # print("Size y" + str(y.size()))
+
+            optimizer.zero_grad
+            with torch.set_grad_enabled(True):
+                outputs = model(x)
+                loss = criterion(outputs, y)
+                loss.backward()
+                optimizer.step()
+                total_loss += loss.item()
+        losses.append(total_loss / (batch_idx + 1))
         if e%500 == 0:
             print("epoch", e, "loss", total_loss)
             learn_rate = learn_rate*learn_rate_decay
